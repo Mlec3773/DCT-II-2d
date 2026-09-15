20cc70e4522a5dc8850d1405cc3e4931a33ee9ab
Mlec3773 <clement.klein@grenoble-inp.org>
2026-09-15 13:33:16 +0200

    comments
    Updated comments in French to English for better clarity.

--- python/buffer-text.py
+++ python/buffer-text.py
@@ -1,10 +1,11 @@
 def generate_file(filename="output.txt", max_number=32, repetitions=32):
     with open(filename, "w") as file:
         for i in range(1, max_number + 1):
-            # Créer une ligne : 1 0 i i i ... i (32 fois)
+            # create a line
+            : 1 0 i i i ... i (32 fois)
             line = ["1", "0"] + [str(i)] * repetitions
-            # Écrire la ligne dans le fichier
+            # Write the line to the file
             file.write(" ".join(line) + "\n")
 
-# Générer le fichier
+# Generate the file
 generate_file()
